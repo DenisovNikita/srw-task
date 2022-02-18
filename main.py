d7a05e414bd5aa5d4d8e2e352e131e08bd705803
Nikita Denisov <nikita160412@yandex.ru>
2022-02-18 15:40:52 +0700

    Improve task 2

--- main.py
+++ main.py
@@ -5,20 +5,35 @@ def binom(n, k):
     return f(n) // (f(k) * f(n - k))
 
 
-def get_value(k, q, p):
-    return p ** (k - q + 1) * (1 - p) ** q - p ** q * (1 - p) ** (k - q + 1)
-
-
-n = 100
-
 p = 0.05
-opt_k, opt_t, opt_s = 0, 0, 0
-for k in range(1, n):
+q0 = 0.05
+q1 = 0.05
+
+
+def get_A_kt(k, t):
+    s = 0
+    for q in range(t, k + 1):
+        s += binom(k, q) * (1 - q1) ** q * q1 ** (k - q)
+    return p * s
+
+
+def get_B_kt(k, t):
+    s = 0
+    for q in range(t, k + 1):
+        s += binom(k, q) * q0 ** q * (1 - q0) ** (k - q)
+    return (1 - p) * s
+
+
+n = 10
+x = 0.6
+
+opt_k, opt_t, opt_pr = 0, 0, 0
+for k in range(1, n + 1):
     for t in range(1, k + 1):
-        s = 0
-        for q in range(t, k + 1):
-            s += binom(k, q) * get_value(k, q, p)
-        if s > opt_s:
-            opt_k, opt_t, opt_s = k, t, s
-        print(f"k = {k}, t = {t}, s = {s}\n")
-print(f"opt_k = {opt_k}, opt_t = {opt_t}, opt_s = {opt_s}\n")
+        num = get_A_kt(k, t)
+        denum = get_A_kt(k, t) + get_B_kt(k, t)
+        pr = num / denum
+        if pr > opt_pr:
+            opt_k, opt_t, opt_pr = k, t, pr
+        print(f"k = {k}, t = {t}, pr = {pr}")
+print(f"opt_k = {opt_k}, opt_t = {opt_t}, opt_pr = {opt_pr}")
